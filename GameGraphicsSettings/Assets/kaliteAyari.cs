c2e3fcc53050af0661919b96138d8a2b40a5613d
Bilal CIFCI <cifci.bilal@outlook.com>
2024-09-18 10:01:37 +0300

    GameGraphisSetting

---
+++ GameGraphicsSettings/Assets/kaliteAyari.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kaliteAyari : MonoBehaviour
+{
+    public Dropdown kaliteDrop;
+    void Start()
+    {
+        
+        if (PlayerPrefs.HasKey("Kalite"))
+        {
+            kaliteDrop.value = PlayerPrefs.GetInt("Kalite");
+            QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("Kalite"));
+
+        }else
+        {
+            PlayerPrefs.SetInt("Kalite", 2);
+            kaliteDrop.value = 2;
+           
+        }
+
+
+    }
+
+    public void KaliteAyari(int kaliteayari)
+    {
+        PlayerPrefs.SetInt("Kalite", kaliteayari);
+
+        QualitySettings.SetQualityLevel(kaliteayari);
+
+     }
+}
