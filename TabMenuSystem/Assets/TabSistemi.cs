a3a6985c8a43e41c96eb122899a6f0fa3404199d
Bilal CIFCI <cifci.bilal@outlook.com>
2024-09-18 05:27:35 +0300

    Tab Menu System

---
+++ TabMenuSystem/Assets/TabSistemi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabSistemi : MonoBehaviour
+{
+    public GameObject[] paneller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButonaBasildi(int index)
+    {
+
+        foreach (var gelenpanel in paneller)
+        {
+            gelenpanel.SetActive(false);
+        }
+
+        paneller[index].SetActive(true);
+
+    }
+}
